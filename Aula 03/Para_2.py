32b4ec73bda5276a32710358969e41fa0fdb9b5d
Richard Pereira <richardpereiraviera@gmail.com>
2024-08-28 20:10:36 -0300

    Laço de Repetição

--- Aula 03/Para_2.py
+++ Aula 03/Para_2.py
@@ -1,2 +1,20 @@
-for X in range (11):
-    print(X)
+alunos = [
+    "Stella",
+    "Sabina",
+    "Kaio",
+    "Richard",
+    "Bruno",
+    "Taylor",
+    "Travis",
+    "Lucas",
+    "Olivia",
+    "Julia"
+]
+alunos.sort()
+alunos.append("Wesley")
+alunos.reverse()
+y = 1
+print(y)
+for x in alunos:
+    print(y,"-",x)
+    y = y + 1
