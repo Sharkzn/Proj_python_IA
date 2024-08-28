dfdbbe79a4c338c8c7764978e2f8234a82abfbbb
Richard Pereira <richardpereiraviera@gmail.com>
2024-08-28 17:41:58 -0300

    Aula de Revisão

---
+++ Revisões/Revisao_2.py
@@ -0,0 +1,5 @@
+Idade = 16
+if(Idade >= 18):
+    print("Você é maior de Idade")
+else:
+    print("você é menor de Idade")
---
+++ Revisões/Revisao_3.py
@@ -0,0 +1,8 @@
+Idade = 20
+if(Idade >= 18):
+    print("Você é Adulto!!!")
+else:
+    if(Idade >= 13):
+        print("Você é Adolecente!!!")
+    else:
+            print("Você é Criança!!!")
