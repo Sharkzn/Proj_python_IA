458bbf2fad17222fa960fb0b4c9676a86e978caf
Richard Pereira <richardpereiraviera@gmail.com>
2024-08-28 20:17:46 -0300

    Laço de repetição

---
+++ Aula 03/Exercício/Exercício.py
@@ -0,0 +1,18 @@
+Frutas = [
+    "Pera",
+    "Laranja",
+    "Manga",
+    "Mexirica",
+    "Pepino",
+    "Uva",
+    "Abacaxi",
+    "Alface",
+    "Tomate",
+    "Repolho"
+]
+Frutas.sort()
+Y = 1
+print(Y)
+for X in Frutas:
+    print(Y,"-",X)
+    Y = Y + 1
