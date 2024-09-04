c300df2f8578ab43ee78dcb26bada742f4cc27bd
Richard Pereira <richardpereiraviera@gmail.com>
2024-09-04 18:24:44 -0300

    Muito Bem!

---
+++ Aula 04/Metodo.py
@@ -0,0 +1,26 @@
+def calcular() :
+    X = 2
+    Y = 2
+    Res = X+Y
+    print(Res)
+
+def Adicao(X,Y):
+    Res = X+Y
+    print(Res)
+
+def subtracao(X,Y):
+    Res = X-Y
+    print(Res)
+
+def multiplicacao(X,Y):
+    Res = X*Y
+    print(Res)
+
+def divisao(X,Y):
+    Res = X/Y
+    print(Res)
+
+Adicao(10,8)
+subtracao(20,2)
+multiplicacao(9,2)
+divisao(36,2)
