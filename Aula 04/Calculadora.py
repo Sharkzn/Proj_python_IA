26acd920a016cd4a2844cae3bb6fff2a3ae43338
Richard Pereira <richardpereiraviera@gmail.com>
2024-09-04 19:55:36 -0300

    Muito Bem!

---
+++ Aula 04/Calculadora.py
@@ -0,0 +1,27 @@
+class Calculadora:
+    def calcular() :
+        X = 2
+        Y = 2
+        Res = X+Y
+        print(Res)
+
+    def Adicao(X,Y):
+        Res = X+Y
+        print(Res)
+
+    def Subtracao(X,Y):
+        Res = X-Y
+        print(Res)
+
+    def Multiplicacao(X,Y):
+        Res = X*Y
+        print(Res)
+
+    def Divisao(X,Y):
+        Res = X/Y
+        print(Res)
+
+    Adicao(10,8)
+    Subtracao(20,2)
+    Multiplicacao(9,2)
+    Divisao(36,2)
---
+++ Aula 04/Main.py
@@ -0,0 +1,7 @@
+from Calculadora import *
+
+class main:
+    Calculadora.Adicao(10,8)
+    Calculadora.Subtracao(20,2)
+    Calculadora.Multiplicacao(9,2)
+    Calculadora.Divisao(36,2)
