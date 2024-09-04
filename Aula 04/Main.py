bf2aaca81639c7b61aed6322c40cbe071c57cf27
Richard Pereira <richardpereiraviera@gmail.com>
2024-09-04 20:58:21 -0300

    Muito Bem!

--- Aula 04/Main.py
+++ Aula 04/Main.py
@@ -2,4 +2,20 @@ from Calculadora import *
 from Lambda import *
 
 class main:
-    x=input("Escolha a sua operação:\n1 - Adição\n2 - Subtração\n3 - Multiplicação\n4 - Divisão")
+    x=int(input("Escolha a sua operação:\n1 - Adição\n2 - Subtração\n3 - Multiplicação\n4 - Divisão\nEscolha: "))
+    if(x==1):
+        A=int(input("Digite o seu Número 1: "))
+        B=int(input("Digite o seu Número 2: "))
+        print(Lambda.Adi(A,B))
+    if(x==2):
+        A=int(input("Digite o seu Número 1: "))
+        B=int(input("Digite o seu Número 2: "))
+        print(Lambda.Sub(A,B))
+    if(x==3):
+        A=int(input("Digite o seu Número 1: "))
+        B=int(input("Digite o seu Número 2: "))
+        print(Lambda.Mul(A,B))
+    if(x==4):
+        A=int(input("Digite o seu Número 1: "))
+        B=int(input("Digite o seu Número 2: "))
+        print(Lambda.Div(A,B))
