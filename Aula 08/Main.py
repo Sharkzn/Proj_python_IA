0097ecfab2e0554fd8904a9fbcf9bbb61928e3c9
Richard Pereira <richardpereiraviera@gmail.com>
2024-10-02 20:56:34 -0300

    Muito Bem!!!

--- Aula 08/Main.py
+++ Aula 08/Main.py
@@ -9,43 +9,51 @@ def ler_csv(file_path):
 
 def treinar(df):
     # Selecionando as colunas de entrada
-    x = df[['Acidez','Densidade','Viscosidade','tonalidade']]
+    x = df[['acidez', 'densidade', 'viscosidade', 'tonalidade']]
     y = df['tipo_de_vinho']
 
-    # Dividindo os dados em dois tipos de treinos 
-    x_train = train_test_split(x,y, test_size=0.2, random_state=42)
-    x_test = train_test_split(x,y, test_size=0.2, random_state=42)
-    y_train = train_test_split(x,y, test_size=0.2, random_state=42)
-    y_test = train_test_split(x,y, test_size=0.2, random_state=42)
+    # Dividindo os dados em conjuntos de treino e teste
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
 
-    # Usando Random como exemplo
-    model= RandomForestClassifier()
-    model.fit(x_train,y_train)
+    # Usando RandomForestClassifier
+    model = RandomForestClassifier()
+    model.fit(x_train, y_train)
 
-    #avaliar modelo e sua precisão
+    # Avaliar modelo e sua precisão
     y_pred = model.predict(x_test)
-    precisão = accuracy_score(y_test,y_pred)
-    print(f"Precisão: {precisão * 100:.2f}%")
+    precisao = accuracy_score(y_test, y_pred)
+    print(f"Precisão: {precisao * 100:.2f}%")
 
     return model
 
 # Prever o vinho
-def prever_vinho(Model,Acidez,Densidade,Viscozidade,Tonalidade):
-    entrada = [[Acidez,Densidade,Viscozidade,Tonalidade]]
-    predicao = Model.predict(entrada)
-    probalidade  = Model.predict_proba(entrada)
+def prever_vinho(model, acidez, densidade, viscosidade, tonalidade):
+   # Criar um Dataframe com o mesmo estrutura usada no trainamento
+    entrada = pd.DataFrame({
+        'acidez': [acidez],
+        'densidade': [densidade],
+        'viscosidade': [viscosidade],
+        'tonalidade': [tonalidade]
+        
+    })
+    predicao = model.predict(entrada)
+    probabilidade = model.predict_proba(entrada)
+    return predicao[0], max(probabilidade[0]) * 100
 
-    return predicao[0],max(probalidade[0])*100
-# Metodo Principal
+# Método Principal
 if __name__ == "__main__":
-    #Variavel que armazena nome do arquivo csv
+    # Variável que armazena nome do arquivo csv
     arquivo = 'vinhos.csv'
     df = ler_csv(arquivo)
     model = treinar(df)
 
-    # Entrada de dados pelo usuario
-    acidez = float(input('Valor de Acidez: '))
-    Densidade = float(input('Valor de Densidade: '))
-    Viscozidade = float(input('Valor de Viscozidade: '))
-    Tonalidade = float(input('Valor de Tonalidade: '))
+    # Entrada de dados pelo usuário
+    acidez = 0.55
+    densidade = 0.85
+    viscosidade = 0.45
+    tonalidade = 0.9
+
+    # Prever tipo de vinho
+    tipo, prob = prever_vinho(model, acidez, densidade, viscosidade, tonalidade)
+    print(f'Tipo de vinho previsto: {tipo}, com probabilidade de {prob:.2f}%')
     
